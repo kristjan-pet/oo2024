8942bafdd4dd5a61a4c905143b6a844c9b821b56
Sa1tamaMan <159195533+Sa1tamaMan@users.noreply.github.com>
2024-02-19 19:02:23 +0200

    kodutoo2 (19.02.2024)

--- 02_klass/kodutoo2/kodutoo/src/Main.java
+++ 02_klass/kodutoo2/kodutoo/src/Main.java
@@ -1,23 +1,4 @@
 public class Main {
-    // public static void main(String[] args) {
-    //     Kasutaja kasutaja = new Kasutaja("M", true, 30, 'x', 0.0);
-    //     Kasutaja kasutaja2 = new Kasutaja("S", false, 35, 'y', 100.0);
-
-    //     kasutaja.muudaAktiivne();
-    //     System.out.println(kasutaja.aktiivne);
-    //     double kasutaja2Raha = kasutaja2.lisaRaha(500.0);
-    //     System.out.println(kasutaja2Raha);
-
-    //     Tellimus tellimus1 = new Tellimus("M", 199.0, "Coca");
-    //     Tellimus tellimus2 = new Tellimus("S", 299.5, "Fanta");
-
-    //     double dollarites = tellimus1.tellimuseSummaDollarites();
-    //     System.out.println(dollarites);
-
-    //     tellimus2.tellijaNimeVahetus("Y");
-    //     System.out.println(tellimus2.tellija);
-    // }
-
     public static void main(String[] args) {
         Item item1 = new Item("6 pack Toilet paper", 100, 12.99);
         Item item2 = new Item("1L Ketchup", 250, 1.79);
